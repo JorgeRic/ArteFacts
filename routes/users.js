fa284e6696f1c5fffa59a330cac0f0d7c2dbba4a
annacv <21217131+annacv@users.noreply.github.com>
2019-07-26 10:41:00 +0200

    [AUTH] Require user model in auth & users route, post method

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {

12d91b2e42d5e7c225aa2ad2072e9386cec0e84d
annacv <21217131+annacv@users.noreply.github.com>
2019-07-27 10:29:12 +0200

    Login, logout OK

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,6 @@ const User = require('../models/User');
 const saltRounds = 10;
 const router = express.Router();
 
-/* GET home page. */
 router.get('/signup', (req, res, next) => {
   if (req.session.currentUser) {
     return res.redirect('/');
@@ -18,29 +17,67 @@ router.get('/signup', (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   const { username, password } = req.body;
+  if (!username || !password) {
+    return res.redirect('/auth/signup');
+  }
+
   const salt = bcrypt.genSaltSync(saltRounds);
   const hashedPassword = bcrypt.hashSync(password, salt);
 
   try {
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.redirect('/auth/signup');
+    }
     const newUser = await User.create({
       username,
       password: hashedPassword
     });
+
     req.session.currentUser = newUser;
     res.redirect('/');
   } catch (error) {
     next(error);
   }
 });
-/*
+
 router.get('/login', (req, res, next) => {
-  res.redirect('index', { title: 'login' });
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
+  res.render('login');
 });
-router.post('/login', (req, res, next) => {
-  res.redirect('index', { title: 'login' });
+
+router.post('/login', async (req, res, next) => {
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.redirect('/auth/login');
+  }
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.redirect('/auth/login');
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res.redirect('/');
+    } else {
+      res.redirect('/auth/login');
+    }
+  } catch (error) {
+    next(error);
+  }
 });
+
 router.post('/logout', (req, res, next) => {
-  res.redirect('index', { title: 'logout' });
+  if (req.session.currentUser) {
+    delete req.session.currentUser;
+    return res.redirect('/');
+  }
+  res.redirect('/');
 });
-*/
+
 module.exports = router;
